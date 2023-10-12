22ede647b58018e862346e5e11ed015e30304f86
Simontagbor <simontagbor360@gmail.com>
2023-10-12 03:42:23 -0700

    add required documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,20 @@ import asyncio
 import random
 
 
+"""
+This module defines a simple function for testing asyncio module and random
+Usage: asyncio.run(wait_random(max_delay: int | default_value=10))
+"""
+
+
 async def wait_random(max_delay: int = 10) -> float:
+
+    """waits for a random amount of seconds and
+    returns the float value of the wait time
+
+    Args: max_delay(int) the maximum delay limit
+    Returns: random_delay(float)
+    """
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
