450f5f720987a5fdf787dd296da9eb303300e2c2
Simontagbor <simontagbor360@gmail.com>
2023-10-12 04:23:01 -0700

    make module documenttion pep8 compliant by adding doctring to the top of the file

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-
-import asyncio
-import random
-
-
 """
 This module defines a simple function for testing asyncio module and random
 
 Function:
 - wait_random(max_delay: int,  default_value=10))
 """
+
+
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
