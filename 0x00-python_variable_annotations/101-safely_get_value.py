7960eade7235e50e3c7c7aab1c43faf47149ca2c
Simontagbor <simontagbor360@gmail.com>
2024-01-14 08:11:22 +0000

    add pep8 update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """Complex types - mixed list"""
 from typing import TypeVar, Dict
-
+from typing import Union, Mapping, Any
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] =
-                     None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
     """Return value of key or default"""
     if key in dct:
         return dct[key]
