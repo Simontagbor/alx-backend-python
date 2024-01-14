daec8fcf8e372cf994531ee2de70e33d22912255
Simontagbor <simontagbor360@gmail.com>
2024-01-14 05:52:03 +0000

    Implement the test_public_repos_url method to unit-test GithubOrgClient._public_repos_url.

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Parameterize and patch as decorators"""
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Class for testing GithubOrgClient"""
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json', return_value={"payload": True})
+    def test_org(self, org_name, mock_get_json):
+        """Test that GithubOrgClient.org returns the correct value."""
+        test_client = GithubOrgClient(org_name)
+        response = test_client.org
+        self.assertEqual(response, {"payload": True})
+        mock_get_json.\
+            assert_called_once_with(
+                    f'https://api.github.com/orgs/{org_name}')
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that GithubOrgClient._public_repos_url
+        returns the expected result."""
+        payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = payload
+        test_client = GithubOrgClient('google')
+        self.assertEqual(test_client._public_repos_url, payload["repos_url"])
