9c97615381f2762646ff8fdad1564655250d3cc5
Simontagbor <simontagbor360@gmail.com>
2024-01-14 06:45:21 +0000

    add type annotated add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""add type annotations to the function"""
+
+
+def add(a: float, b: float) -> float:
+    """add two floats and
+    return the result as a float"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,2 @@
+# 0x00. Python - Variable Annotations
+In this project I learned about Python type annotations.
