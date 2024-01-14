c36de4bc775f15f6db566c1fb280653a027729f7
Simontagbor <simontagbor360@gmail.com>
2024-01-14 07:50:49 +0000

    Duck type annotate thr function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module contains the function for task 100"""
-from typing import List, Any, Optional
+from typing import Sequence, Any, Optional
 
 
-def safe_first_element(lst: List[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """This function returns the first element of a list"""
     if lst:
         return lst[0]
