efbf020ab918efea45f197c536f3c9b7c9640f34
Simontagbor <simontagbor360@gmail.com>
2024-01-14 06:54:18 +0000

    add function to calculate the floor of a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    The floor of a float is the largest
+    integer less than or equal to the float.
+
+    Parameters:
+    n (float): The float to floor.
+
+    Returns:
+    int: The floor of the float.
+    """
+    return math.floor(n)
